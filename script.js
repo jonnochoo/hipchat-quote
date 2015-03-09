58c28485fd4e0d587dc0d9a467fb5d994144b546
Jonathon Choo <jonno.choo@gmail.com>
2015-03-09 23:45:48 +1100

    Initial version

---
+++ script.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var format = require('string-template');
+var Hipchatter = require('hipchatter');
+var quoteUrl = 'http://quotes.jonnochoo.com/api/quotes/random';
+
+var roomName = process.argv[2];
+var hipchatAuthToken = process.argv[3];
+var hipchatRoomToken = process.argv[4];
+
+request.get(quoteUrl, function(err, res, data) {
+  if(err) {
+    throw err;
+  }
+
+  var quote = JSON.parse(data);
+  var quoteText = quote.author ? format("{text} - {author}", quote) : text;
+
+  var hipchatter = new Hipchatter(hipchatAuthToken);
+  hipchatter.notify(roomName, { message: quoteText, color: 'green', token: hipchatRoomToken }, 
+    function(err){
+      if(err)
+        console.log(err);
+
+      if (err == null) 
+        console.log('Successfully notified the room.');
+  });
+});
